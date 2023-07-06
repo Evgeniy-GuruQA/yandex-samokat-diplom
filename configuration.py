70b341eec6659d01ad7daccb4eecc4b34e3759f3
EvgeniyEliseev <135517201+Evgeniy-GuruQA@users.noreply.github.com>
2023-07-06 14:29:21 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://cab33900-3637-496a-8b93-f04fbd1a7bc9.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Иван",
+    "lastName": "Иванович",
+    "address": "Москва, проспект Мира, 38",
+    "metroStation": 99,
+    "phone": "+79993456789",
+    "rentTime": 3,
+    "deliveryDate": "2023-06-05",
+    "comment": "Позвоните за час до прибытия",
+    "color": [
+        "BLACK"
+    ]
+}
